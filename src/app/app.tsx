a1cc5e56f101764239126022a0cde319804c41e5
Eric Chong <hoichieric@hotmail.com>
2024-07-03 13:36:00 -0700

    Add list tickets and ticket details routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,21 @@
-import './app.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Tickets from "src/routes/Tickets";
+import Ticket from "src/routes/Ticket";
+import "./app.css";
 
 export type AppProps = {};
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="tickets" element={<Tickets />} />
+          <Route path="tickets/:ticketId" element={<Ticket />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/routes/Ticket/Ticket.tsx
@@ -0,0 +1,3 @@
+export default function Ticket() {
+  return <h1>Ticket detail page</h1>;
+}
